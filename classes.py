44c233c3f1f282a9cf3bc73a2d11702946bebbcb
Erfan Rajati Haghi <main.e.rajati@gmail.com>
2024-12-17 20:28:11 +0330

    New Branch set up

---
+++ classes.py
@@ -0,0 +1,9 @@
+class Example:
+    def __init__(self):
+        pass
+
+class Topic:
+    def __init__(self):
+        self.name = str()
+        self.examples: list[Example] # Dynamically generated from Database
+
